34c22ab37fc4c133d80f4e68f139a7227e3cd378
vtuanjs <vantuan130393@gmail.com>
2025-11-25 20:09:03 +0700

    feat: update cicd script

--- apps/saas_mngt_service/cicd/build_push.sh
+++ apps/saas_mngt_service/cicd/build_push.sh
@@ -5,7 +5,6 @@
 
 IMAGE_TAG=${IMAGE_TAG:-latest}
 DOCKER_REPOSITORY="${DOCKER_REPOSITORY:-vantuan1303/saas_mngt_service:$IMAGE_TAG}"
-REMOTE_CACHE="${REMOTE_CACHE:-http://bazel:bazel@localhost:8080}"
 
 if [ -e ".build" ]; then
 	echo "Removing existing .build..."
@@ -13,7 +12,7 @@ if [ -e ".build" ]; then
 fi
 
 # You need to install Docker and Bazel before running this script.
-bazel build --remote_cache=$REMOTE_CACHE --remote_upload_local_results //cmd:cmd
+bazel build //cmd:cmd
 
 bazel build //cmd:image
 
